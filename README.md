93aad75847346c8f593248d0bdc86ed7b169a2a8
Arthur Gordon-Wright <agordonwright@gmail.com>
2019-08-30 16:36:00 +1200

    Add copyright notices for >GPLv3 and version info

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# simplequi
+# simplequi v0.0.1
 Run codeskulptor.org programs on the desktop using Qt
--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
+"""
+Copyright ©2019 Arthur Gordon-Wright
+
+This file is part of simplequi.
+
+simplequi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+simplequi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with simplequi.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from os import path
 from setuptools import setup
 
@@ -27,9 +46,9 @@ setup(
     install_requires=[],
     extras_require={
         'dev': [
-            'wheel',
-            'setuptools',
-            'bumpversion',
+            'bumpversion==0.5.3',
+            'setuptools==41.2.0',
+            'wheel==0.33.6',
         ],
     },
 )
