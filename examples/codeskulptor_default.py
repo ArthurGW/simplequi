18df7b816736d024c584af873d024ccbe0c12996
Arthur Gordon-Wright <agordonwright@gmail.com>
2019-09-01 11:37:22 +1200

    Clarify that the codeskulptor example script is not Copyright by me

--- examples/codeskulptor_default.py
+++ examples/codeskulptor_default.py
@@ -1,39 +1,26 @@
-# -----------------------------------------------------------------------------
-# Copyright ©2019 Arthur Gordon-Wright
-# <https://github.com/ArthurGW/simplequi>
-# <simplequi.codeskulptor@gmail.com>
-# 
-# This file is part of simplequi.
-# 
-# simplequi is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# simplequi is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with simplequi.  If not, see <https://www.gnu.org/licenses/>.
-# -----------------------------------------------------------------------------
+# NOTE: This script is the default script on loading Codeskulptor, included here as a basic smoke test to target dev
+# efforts towards.  See https://py3.codeskulptor.org/about.html for details of the creation of Codeskulptor itself,
+# including this script.  The copyright in this script is not held by the simplequi creator.
+
+# CodeSkulptor runs Python programs in your browser.
+# Click the upper left button to run this simple demo.
+
+# CodeSkulptor is tested to run in recent versions of
+# Chrome, Firefox, and Safari.
+
 
 import simplequi as simplegui
 
 message = "Welcome!"
-
 
 # Handler for mouse click
 def click():
     global message
     message = "Good job!"
 
-
 # Handler to draw on canvas
 def draw(canvas):
     canvas.draw_text(message, [50,112], 48, "Red")
-
 
 # Create a frame and assign callbacks to event handlers
 frame = simplegui.create_frame("Home", 300, 200)
