b57a1b44ec9a6bde2b7de93f42fafef7e2cf1486
Arthur Gordon-Wright <agordonwright@gmail.com>
2019-10-19 19:54:08 +1300

    Bump version: 0.8.0 → 0.9.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2019, Arthur Gordon-Wright'
 author = 'Arthur Gordon-Wright'
 
 # The full version, including alpha/beta/rc tags
-release = '0.8.0'
+release = '0.9.0'
 
 
 # -- General configuration ---------------------------------------------------
