15e2276331efe9d80b5116edb93420d8f9e6d86d
Arthur Gordon-Wright <agordonwright@gmail.com>
2019-12-17 21:38:34 +1300

    Bump version: 1.0.3 → 1.0.4

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # simplequi
 
-*v1.0.3*
+*v1.0.4*
 
 ![Build Status](https://github.com/ArthurGW/simplequi/workflows/build/badge.svg)
 ![Test Status](https://github.com/ArthurGW/simplequi/workflows/tests/badge.svg)
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,7 @@ copyright = '2019, Arthur Gordon-Wright'
 author = 'Arthur Gordon-Wright'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.3'
+release = '1.0.4'
 
 
 # -- General configuration ---------------------------------------------------
